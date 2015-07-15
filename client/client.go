543d6de12f30fdda109c594263293a8ea6053dd5
vzaramel <vinicius.zaramella@gmail.com>
2015-07-14 23:41:28 -0300

    commit inicial para elasticpush-go

---
+++ client/client.go
@@ -0,0 +1,102 @@
+package client
+
+import (
+    "net/http"
+    "strings"
+    "errors"
+    "encoding/json"
+    // "fmt"
+    "io/ioutil"
+)
+
+const API_VERSION = "v1";
+const HOST = "https://api.elasticpush.com/"
+
+type Client struct{
+    token string
+    tokenSecret string
+    apiId string
+    url string
+    clientId string
+}
+
+type HttpResponse struct{
+    Body []byte
+    Code int
+}
+
+func (r *HttpResponse) GetBody() []byte{
+    return r.Body
+}
+
+func (r *HttpResponse) GetCode() int{
+    return r.Code
+}
+
+func New( token, apiId string) (*Client, error){
+    url := HOST + API_VERSION + "/apps/" + apiId
+    splittedToken := strings.Split(token, ":")
+    if len(splittedToken) != 2 {
+        return nil, errors.New("Invalid token format")
+    }
+    c := &Client{ splittedToken[0], splittedToken[1], apiId, url, ""}
+    return c, nil
+}
+
+func (c *Client) SetClientId( id string) {
+    c.clientId = id
+}
+
+type Reader struct {
+    Data []byte
+}
+
+func (r *Reader) Read(p []byte) (n int, err error){
+    p = r.Data
+    return len(p), nil
+}
+
+func (c *Client) Dispatch( channel, event string, data interface{}) (*HttpResponse, error){
+
+    requestBody := make(map[string]interface{})
+    requestBody["channel"] = channel;
+    requestBody["event"] = event;
+    requestBody["data"] = data;
+    if c.clientId != ""{
+        requestBody["identifier"] = c.clientId
+    }
+
+    requestBodyJson, err := json.Marshal(requestBody)
+    if err != nil{
+        return nil, err
+    }
+
+    // fmt.Printf("%s",c.url + "/events");
+    // fmt.Printf("%s",requestBodyJson);
+    reader := &Reader{requestBodyJson}
+
+    req, err := http.NewRequest("POST", c.url +"/events" , reader)
+    if err != nil {
+        return nil, err
+    }
+
+    req.Header.Set("X-Token", c.token)
+    req.Header.Set("X-Secret-Token", c.tokenSecret)
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("Accept", "application/json")
+
+    // Send the request via a client
+    client := &http.Client{}
+
+    // fmt.Printf("%+v", req)
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    if body, err := ioutil.ReadAll(resp.Body); err == nil {
+        return &HttpResponse{body,resp.StatusCode}, nil
+    }
+    return nil, err
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "github.com/Elasticpush/elasticpush-go/client"
+    "fmt"
+)
+
+func main(){
+
+    token := "9751998fa08dea907624a815270d294118e9fc5c85be1453bd4f338acf467e01";
+    tokenSecret := "d7cb880a62ec0a6910cb449d5c6ca60cad8e233f171ed53f2b91ff88bf99cff3";
+    c, err := client.New( token + ":" + tokenSecret , "11")
+    if  err != nil {
+        panic("Crashed");
+    }
+    c.SetClientId("06376146296970546")
+
+    data := make(map[string]string)
+    data["message"] = "Teste"
+
+    httpResp, err := c.Dispatch("channel-0346", "event-test", data);
+    if  err != nil {
+        panic("Crashed Hard");
+    }
+    fmt.Printf("%s", httpResp.GetBody())
+}
